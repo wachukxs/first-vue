e66a24961375ab212526aa2d23ed1ac87a2bf370
wachukxs <nwachukwuossai@gmail.com>
2020-07-17 01:15:34 +0100

    matching heights

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="container">
-        <div class="uk-child-width-expand@s uk-text-center" uk-grid>
+    <div class="container uk-margin uk-margin-right uk-margin-left">
+        <div class="uk-child-width-expand@s uk-text-center uk-grid-match" uk-height-match="target: > div > .uk-card" uk-grid>
             <div>
-                <div class="uk-card uk-card-default uk-card-body">Item</div>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../assets/images/image-4.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <p>do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
             </div>
             <div>
-                <div class="uk-card uk-card-default uk-card-body">Item</div>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../assets/images/image-3.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <p>do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
             </div>
             <div>
-                <div class="uk-card uk-card-default uk-card-body">Item</div>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../assets/images/image-2.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <p>do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
             </div>
 
             <div>
@@ -28,7 +49,7 @@
 
 <script>
 export default {
-    name: 'Index'
+    name: 'Index',
 }
 </script>
 
