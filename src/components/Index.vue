9fadb2b35f8fccf898a3bea9b2224b27c7eb43b2
wachukxs <nwachukwuossai@gmail.com>
2020-07-17 01:56:05 +0100

    nav bar

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,13 +1,41 @@
 <template>
     <div class="container uk-margin uk-margin-right uk-margin-left">
-        <div class="uk-child-width-expand@s uk-text-center uk-grid-match" uk-height-match="target: > div > .uk-card" uk-grid>
+
+        <nav class="uk-navbar-container" uk-navbar>
+            <div class="uk-navbar-left">
+
+                <ul class="uk-navbar-nav">
+                    <li class="uk-active"><a href="#">Home</a></li>
+                    <li>
+                        <a href="#">Sellers</a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="#">Login</a></li>
+                                <li><a href="#">Sign up</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">Buyers</a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="#">Interests</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+
+        <div class="uk-margin-top uk-child-width-expand@s uk-text-center uk-grid-match" uk-height-match="target: > div > .uk-card" uk-grid>
             <div>
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-media-top">
                         <img src="../assets/images/image-4.jpg" alt="">
                     </div>
                     <div class="uk-card-body">
-                        <p>do eiusmod tempor incididunt.</p>
+                        <p>Some details about this product.</p>
 
                         <div class="uk-margin-small">
                             <div class="uk-button-group">
@@ -41,7 +69,7 @@
                         <img src="../assets/images/image-3.jpg" alt="">
                     </div>
                     <div class="uk-card-body">
-                        <p>do eiusmod tempor incididunt.</p>
+                        <p>Some details about this product.</p>
 
                         <div class="uk-margin-small">
                             <div class="uk-button-group">
@@ -58,7 +86,7 @@
                         <img src="../assets/images/image-2.jpg" alt="">
                     </div>
                     <div class="uk-card-body">
-                        <p>do eiusmod tempor incididunt.</p>
+                        <p>Some details about this product.</p>
 
                         <div class="uk-margin-small">
                             <div class="uk-button-group">
@@ -76,7 +104,7 @@
                         <img src="../assets/images/image-1.jpg" alt="">
                     </div>
                     <div class="uk-card-body">
-                        <p>do eiusmod tempor incididunt.</p>
+                        <p>Some details about this product.</p>
 
                         <div class="uk-margin-small">
                             <div class="uk-button-group">
